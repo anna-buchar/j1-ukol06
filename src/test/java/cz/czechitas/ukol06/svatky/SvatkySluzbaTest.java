44b022f82531eb65e6f687d90582d288758f71c6
Anna <anickab99@gmail.com>
2025-06-07 18:21:54 +0200

    test

--- src/test/java/cz/czechitas/ukol06/svatky/SvatkySluzbaTest.java
+++ src/test/java/cz/czechitas/ukol06/svatky/SvatkySluzbaTest.java
@@ -2,12 +2,21 @@ package cz.czechitas.ukol06.svatky;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.time.MonthDay;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SvatkySluzbaTest {
 
     @Test
-    void vyhledatSvatkyKeDni() {
+    void vyhledatSvatkyKeDni() throws IOException {
         //TODO implementovat test metody vyhledatSvatkyKeDni
+            SvatkySluzba seznamSvatku = new SvatkySluzba();
+            assertEquals(List.of("Diana"), seznamSvatku.vyhledatSvatkyKeDni(MonthDay.of(1, 4)));
+            assertEquals(List.of("Adam", "Eva", "Gaia", "Kračun"), seznamSvatku.vyhledatSvatkyKeDni(MonthDay.of(12, 24)));
+            //assertNull(seznamSvatku.vyhledatSvatkyKeDni(MonthDay.of(2, 30)));
+            //den bez svatku neni:(
     }
 }
